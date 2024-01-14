2b2aad3b16dd6b4e14fb94fcfb31fac42bd3f2cf
MineItRiGrEight <80468882+MineItRiGrEight@users.noreply.github.com>
2024-01-14 11:37:27 +0100

    feat: Add Arch Linux dotfiles

---
+++ hypr/.config/hypr/autostart.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+setxkbmap se
+/usr/lib/polkit-kde-authentication-agent-1 &
+hyprpaper &
+waybar &
+swaync &
+udiskie -t &
+avizo-service &
+hyprpm reload -n
+ulauncher --no-window-shadow --hide-window --dev &
+xwaylandvideobridge &
+#brave &
+dex -a &
