c17799a39a035733a4beb476c682c8ed3828e58e
MineItRiGrEight <mirge@abisko.local>
2024-02-26 18:26:45 +0100

    feat(terminals): Add wezterm config

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-stow zsh kitty nvim helix zellij hyprland rofi waybar tmux
+stow zsh kitty nvim helix zellij hyprland rofi waybar tmux wezterm
---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.color_scheme = "rose-pine"
+
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+config.font_size = 13
+
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+
+return config
