9fa59d5dcba9fe6f057a534cbc8544069c464963
MineItRiGrEight <mirge@abisko.local>
2024-03-07 17:13:59 +0100

    fix(wezterm): Remove font setting
    Fixes missing icon issue, since WezTerm already bundles Nerd Font symbols and Noto Color Emoji

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,7 +3,6 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "rose-pine"
 
-config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 config.font_size = 13
 
 config.hide_tab_bar_if_only_one_tab = true
