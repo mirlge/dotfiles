ca920565ef7521d7ddb64cb03abcb76af7de1efd
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2024-04-26 20:21:07 +0200

    feat: Add system wide configuration files
    For now it's only for `keyd`

---
+++ _system/unsetup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# WARNING: This can be dangerous if used in certain ways!
+echo 'WARNING: This can be dangerous if used in certain ways!'
+echo
+printf 'Are you sure you want to do this? (y/N) '
+read -r danger_answer
+if [ "$danger_answer" != "y" ]; then
+	exit
+fi
+
+stow -t / -D keyd
