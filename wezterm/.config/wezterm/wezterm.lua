07a9aaa9d3b95b8fd304d0909506af45d00d0d96
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2024-11-29 17:57:25 +0100

    fix(wezterm): Don't resize window when changing font size
    Caused issues with GlazeWM in Windows

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,6 +5,7 @@ local theme_plugin = wezterm.plugin.require("https://github.com/neapsix/wezterm"
 config.colors = theme_plugin.main.colors()
 
 config.font_size = 12
+config.adjust_window_size_when_changing_font_size = false
 
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
