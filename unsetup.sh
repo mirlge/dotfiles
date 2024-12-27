ee305cd0f3eed80a0f7c9816a8553d7d507bd7f9
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2024-12-27 18:37:56 +0100

    feat: Add meteor dotfiles

--- unsetup.sh
+++ unsetup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-stow -D zsh kitty nvim helix zellij hyprland rofi waybar tmux wezterm ghostty
+stow -D zsh kitty nvim helix zellij hyprland rofi waybar tmux wezterm ghostty meteor
