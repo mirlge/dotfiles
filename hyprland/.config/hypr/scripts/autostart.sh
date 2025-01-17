e28c9b13c3cb6bff07e359bada598d3cb2dccbf3
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2025-01-17 22:15:38 +0100

    revert: refactor(hyprland)!: Switch Alt-Tab program to [hyprswitch](https://github.com/H3rmt/hyprswitch)
    This reverts commit 460ec567e553ee17d09ff0c08bbc3f61d1e1e662
    
    Rofi's window picker has more information

--- hyprland/.config/hypr/scripts/autostart.sh
+++ hyprland/.config/hypr/scripts/autostart.sh
@@ -10,6 +10,5 @@ swayosd-server &
 avizo-service &
 hyprpm reload -n
 batsignal &
-hyprswitch init
 ulauncher --no-window-shadow --hide-window --dev &
 systemctl --user start dex-autostart.service &
