c968fabfd35203f4d6ebd4dcea9d594732bebb07
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2025-04-05 09:21:48 +0200

    feat: Add `wl-kbptr` config
    By default, `wl-kbptr`'s 'floating' mode takes input from stdin rather than auto-detecting

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-stow zsh kitty nvim helix zellij hyprland rofi waybar tmux wezterm ghostty meteor
+stow zsh kitty nvim helix zellij hyprland rofi waybar tmux wezterm ghostty meteor wl-kbptr
--- unsetup.sh
+++ unsetup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-stow -D zsh kitty nvim helix zellij hyprland rofi waybar tmux wezterm ghostty meteor
+stow -D zsh kitty nvim helix zellij hyprland rofi waybar tmux wezterm ghostty meteor wl-kbptr
