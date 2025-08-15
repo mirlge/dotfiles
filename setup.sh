340db44a63128f39544dc62bc44c0343cc7aa9c2
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2025-08-15 11:44:11 +0200

    fix(setup): Use `#!/usr/bin/env sh` instead of `#!/usr/bin/env bash` in setup.sh and unsetup.sh
    Makes it possible to use without bash

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 stow zsh kitty nvim helix zellij hyprland rofi waybar tmux wezterm ghostty meteor wl-kbptr kakoune
--- unsetup.sh
+++ unsetup.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 stow -D zsh kitty nvim helix zellij hyprland rofi waybar tmux wezterm ghostty meteor wl-kbptr kakoune
