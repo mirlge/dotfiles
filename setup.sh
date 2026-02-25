e1381d52c0975ce5802fa3d117fe437ee84a471d
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2026-02-25 21:52:23 +0100

    feat: Add wlr-which-key configuration

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-stow zsh kitty nvim helix zellij hyprland rofi waybar tmux wezterm ghostty meteor wl-kbptr kakoune vim matugen
+stow zsh kitty nvim helix zellij hyprland rofi waybar tmux wezterm ghostty meteor wl-kbptr kakoune vim matugen wlr-which-key
--- unsetup.sh
+++ unsetup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-stow -D zsh kitty nvim helix zellij hyprland rofi waybar tmux wezterm ghostty meteor wl-kbptr kakoune vim matugen
+stow -D zsh kitty nvim helix zellij hyprland rofi waybar tmux wezterm ghostty meteor wl-kbptr kakoune vim matugen wlr-which-key
