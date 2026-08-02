da14639fa4fb4c5a0e51dd03bb97f0718334b9cd
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2026-08-02 15:45:55 +0200

    fix(hyprland): Make the theme restart script set the wallpaper, using Waypaper, unless KEEP_WAYPAPER is set

--- hyprland/.config/hypr/scripts/theme-restart.sh
+++ hyprland/.config/hypr/scripts/theme-restart.sh
@@ -6,5 +6,6 @@ if [ -n "$error_code" ]; then
   hyprctl reload
   exit "$error_code"
 fi
-hyprctl dispatch "hl.dsp.exec_cmd('KEEP_WAYPAPER=\"$KEEP_WAYPAPER\" \"$(dirname "$0")/restart_shell.sh\"')"
+[ -z "$KEEP_WAYPAPER" ] && waypaper --wallpaper "$1"
+hyprctl dispatch "hl.dsp.exec_cmd('KEEP_WAYPAPER=1 bash \"$(dirname "$0")/restart_shell.sh\"')"
 hyprctl reload
