97a43d3d8900007cefcd3c66ced281c36542aa34
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2026-08-14 12:28:39 +0200

    fix(hyprland): Allow hyprpicker (/usr/bin/hyprpicker) access to the Wayland screencopy protocol without having to ask for permission

--- hyprland/.config/hypr/hyprland.lua
+++ hyprland/.config/hypr/hyprland.lua
@@ -147,6 +147,7 @@ hl.layer_rule({ match = { namespace = "swaync-control-center" }, animation = "sl
 
 hl.permission({ binary = "/usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland", type = "screencopy", mode = "allow" })
 hl.permission({ binary = "/usr/bin/hyprlock", type = "screencopy", mode = "allow" })
+hl.permission({ binary = "/usr/bin/hyprpicker", type = "screencopy", mode = "allow" })
 hl.permission({ binary = "/usr/bin/wl-kbptr", type = "screencopy", mode = "allow" })
 
 hl.animation({ leaf = "workspaces", enabled = true, speed = 8, bezier = "default", style = "slidevert" })
