4a4e30604b230e2f5f2c78f3172920a94118ae6c
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2026-08-19 17:45:48 +0200

    refactor(wezterm): Decrease font size from 12 to 11, to match Ghostty

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "iris"
 
-config.font_size = 12
+config.font_size = 11
 config.adjust_window_size_when_changing_font_size = false
 
 config.hide_tab_bar_if_only_one_tab = true
